7e9a430e0dcf04dc721bb063676af29815aaf972
josephbin811 <672631162@qq.com>
2018-07-11 12:21:55 +0800

    專案完成
    專案完成

---
+++ HW707/Controllers/MyHomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HW707.Models.ViewModels;
+
+
+namespace HW707.Controllers
+{
+    public class MyHomeController : Controller
+    {
+        // GET: MyHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult SubCashFlow()
+        {
+            #region 做虛擬資料
+
+            string theType = null;
+            string theDate = null;
+            int theAmount = 0;
+
+            List<CashFlowModel> list = new List<CashFlowModel>();
+
+            for (int i = 0; i < 50; i++)
+            {
+                Random ran = new Random(Guid.NewGuid().GetHashCode());
+
+                if (i % 2 == 0)
+                    theType = "收入";
+                else
+                    theType = "支出";
+
+                theDate = "2018" + "-" + (ran.Next(1, 12)).ToString() + "-" + (ran.Next(1, 28)).ToString();
+
+                theAmount = ran.Next(100, 5000);
+
+                CashFlowModel obj = new CashFlowModel
+                {
+                    flowType = theType,
+                    flowDate = theDate,
+                    flowAmount = theAmount.ToString()
+                };
+
+                list.Add(obj);
+            }
+
+            ViewData["moneyFlowList"] = list;
+            #endregion
+
+            return View();
+        }
+    }
+}
---
+++ HW707/Models/ViewModels/CashFlowModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HW707.Models.ViewModels
+{
+    public class CashFlowModel
+    {
+        public string flowType { get; set; }
+        public string flowDate { get; set; }
+        public string flowAmount { get; set; }
+    }
+}
